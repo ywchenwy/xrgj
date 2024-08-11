5b16449372085f3cbda29adf44d9e6ea890d2660
cyw <2284344083@qq.com>
2024-08-11 16:05:23 +0800

    perf: 优化体验，回到顶部的动画的速度加快

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@
     }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 200, 'easeInOutExpo');
         return false;
     });
 

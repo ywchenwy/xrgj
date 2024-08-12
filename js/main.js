790612bb094d80b146041ca1ca174a3786f64921
cyw <2284344083@qq.com>
2024-08-12 11:36:48 +0800

    feat: product页面的navbar不进行固定顶部

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@
     // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 45) {
-            $('.navbar').addClass('sticky-top shadow-sm');
+            $('.navbar:not(.product .navbar)').addClass('sticky-top shadow-sm');
         } else {
-            $('.navbar').removeClass('sticky-top shadow-sm');
+            $('.navbar:not(.product .navbar)').removeClass('sticky-top shadow-sm');
         }
     });
 
